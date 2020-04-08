cd086746eb97e7ba29c03e1e86597de2ceb1b44f
bowenzhao <bzhao19@ucsc.edu>
2020-04-08 18:47:25 -0400

    Scence & asset implemented

---
+++ src/scences/Play.js
@@ -0,0 +1,32 @@
+class Play extends Phaser.Scene{
+    constructor() {
+        super("playScene");
+    }
+
+    preload(){
+        //load img/title sprite
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create(){
+        //place title sprite
+        this.starfield = this.add.titleSprite(0, 0, 640, 480, 'starfield');
+
+        //white rectangle borders
+        this.add.rectangle(5, 5, 630, 32, 0xFACADE).setOrigin(0, 0);
+        this.add.rectangle(5, 443, 630, 32, 0xFACADE).setOrigin(0, 0);
+        this.add.rectangle(5, 5, 32, 455, 0xFACADE).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFACADE).setOrigin(0, 0);
+
+
+        //green UI background
+        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+    }
+
+    update(){
+        //scroll starfield
+        this.starfield.tilePositionX -= 4;
+    }
+}
